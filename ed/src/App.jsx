f32c887ed88fceb7f851c7d7d210933ed4ca3af5
dpqlsn <a01037111209@gmail.com>
2025-05-12 10:47:11 +0900

    feat(#07) :: 이동

--- ed/src/App.jsx
+++ ed/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/new" element={<New />} />
-      <Route path="/Edit" element={<Edit />} />
+      <Route path="/edit/:id" element={<Edit />} />
     </Routes>
   )
 }
